905bc27a5ff38b7a21c36d63896c4a28163b0c5c
kun <coco445566@163.com>
2018-11-30 15:32:57 +0800

    memcached守护进程脚本

---
+++ rc_memcached_daemon.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+from subprocess import Popen, PIPE   ##插入subprocess包两个模块 用于执行命令
+import os
+import sys
+
+class Process(object):
+    
+    args = {'PORT':11211,'USER':'memcached','MAXCONN':1024,'CACHESIZE':64,'OPTIONS':''}  #定义开始默认参数
+
+    def __init__(self, name, program, workdir):  #实例化自动加载该方法 name命令名字 program 执行文件路径
+        self.name = name                               #workdir PID目录
+        self.program = program                         #类的属性接受参数
+        self.workdir = workdir
+
+#'''  读取memcached配置文件'''
+    def readConf(self,f):                         #读取memcached配置文件 /ect/sysconfig/memcached内容
+        with open(f) as fd:                       #并以该文件内容为最新的参数以字典形式返回
+            lines = fd.readlines()
+            return dict([i.strip().replace('"','').split('=') for i in lines]) #strip去空格 replace去” split去=
+
+#''''  替换默认参数'''
+    def replaceArgs(self):
+        conf = self.readConf('/etc/sysconfig/memcached')  #/ect/sysconfig/memcached内的参数替换掉默认参数并返回
+        if 'PORT' in conf: 
+            self.args['PORT'] = conf['PORT']
+        if 'USER' in conf:
+            self.args['USER'] = conf['USER']
+        if 'MAXCONN' in conf:
+            self.args['MAXCONN'] = conf['MAXCONN']
+        if 'CACHESIZE' in conf:
+            self.args['CACHESIZE'] = conf['CACHESIZE']
+        options = ['-p',self.args['PORT'],
+                   '-u',self.args['USER'],
+                   '-m',self.args['MAXCONN'],
+                   '-c',self.args['CACHESIZE'] ]
+        os.system('chown %s %s' %(self.args['USER'], self.workdir))  #更改var/tmp/memcached权限 来创建该目录
+        return options
+
+#'''  自定义初始化方法 '''   
+    def pidDir(self):
+        if not os.path.exists(self.workdir):     ##判断自定义的PID目录var/tmp/memcached不存在，并创建进入该目录
+            os.mkdir(self.workdir)
+            os.chdir(self.workdir) 
+
+#'''  创建memcached的PID文件 '''
+    def pidFile(self):                             ##返回memcachedPID的文件路径 var/tmp/memcached/memcached.pid
+        return os.path.join(self.workdir,'%s.pid' %self.name)
+
+#'''  把PID的数值写入PID文件中 '''
+    def writePid(self):
+        if self.pid:                               #判断pid存在并把pid写入var/tmp/memcached/memcached.pid中
+            with open(self.pidFile(),'w') as fd:
+                fd.write(str(self.pid))
+
+#'''  定义start方法 '''
+    def start(self):
+        pid = self.getPid()
+        if pid:
+            print '%s 已经在跑了...' %self.name      #进程已经有了再此运行start会退出
+            sys.exit()
+        self.pidDir()                                #调用pidDir方法创建目录
+        cmd = [self.program] + self.replaceArgs() + ['-d','-P',self.pidFile()]
+        print cmd                                    #打印出命令内容
+        p = Popen(cmd, stdout=PIPE)                  #命令通过管道输出
+        #self.pid = p.pid                            #得到pid数值并定义对象公有属性 让weiterPid方法可以调用 
+        #self.writePid()                             #调用writePid方法 -P会自动把pid写进文件中因此不需要这两句
+        print '%s 启动成功...' %self.name
+
+#''' 获得memcached的pid '''
+    def getPid(self):
+        p = Popen(['pidof',self.name],stdout=PIPE)   #获得pidof memcached命令来得到pid
+        pid = p.stdout.read().strip()                #strip()去\n
+        return pid
+
+#'''  定义stop方法 '''
+    def stop(self):
+        pid = self.getPid()                               #调用getPid方法获取pid
+        if pid:
+            os.kill(int(pid), 15)                         #pid存在就傻掉pid
+            if os.path.exists(self.pidFile()):            #再删除/tmp/memcached/memcached.pid
+                os.remove(self.pidFile())
+            print '%s 正在关闭...' %self.name
+
+#'''  定义restart方法 '''
+    def restart(self):
+        self.stop()
+        self.start()
+
+#'''  定义status方法 '''
+    def status(self):
+        pid =self.getPid()
+        if pid:
+            print '%s 已经在运行中...' %self.name
+        else:
+            print '%s 没有运行...' %self.name
+
+#'''  定义status方法  '''
+    def helps(self):
+        print '你可以按以下格式输入 %s {start|stop|restart|status}' %__file__ #__file__表示脚本本身
+
+def main():       ##定义一个主函数
+    
+    name = 'memcached'
+    prog = '/usr/bin/memcached'
+    wd = '/var/tmp/memcached'
+    pm = Process(name = name, program = prog, workdir = wd)   ##实例化对象 并传参
+    
+    try:                   ##抓取脚本后是否带上选项,没有退出
+        cmd = sys.argv[1]
+    except IndexError:
+        print '没有带上选项'
+        sys.exit()
+
+    if cmd == 'start':     ##判断选项来调用对象对应的方法
+        pm.start()
+
+    elif cmd == 'stop':
+        pm.stop()
+
+    elif cmd == 'restart': 
+        pm.restart()
+
+    elif cmd == 'status':
+        pm.status()
+
+    else:
+        pm.helps()
+
+
+if __name__ == '__main__':   ##当自己执行自己会调用主函数 当被其他脚本作为模块时就不调用主函数
+    main()
